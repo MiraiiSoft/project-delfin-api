0fdbae2c6f528d3aee986c138c1ea5ba224c340f
ErickA2000 <erickwladimirarevalonavarro@gmail.com>
2023-06-22 10:06:47 -0600

    realizado servicio de subida de images

--- src/services/firebase.js
+++ src/services/firebase.js
@@ -1,10 +1,33 @@
 import firebase from "firebase/compat/app";
 import "firebase/compat/storage";
 import { getStorage, ref, deleteObject } from "firebase/storage";
+import { firebaseConfig } from "../config.js";
+
+firebase.initializeApp(firebaseConfig);
 
 const storageRef = firebase.app().storage().ref();
 const storage = getStorage();
 
-export async function uploadImg( nameFolde, nameImg, imgBase64 ){
+export async function uploadImg( nameFolder, nameImg, imgBase64 ){
+    try {
+        let response = await storageRef.child( nameFolder+"/"+nameImg )
+            .putString(imgBase64, "data_url");
+        
+        return await response.ref.getDownloadURL();
+    } catch (error) {
+        console.log("Service upload img error: ", error);
+        return error;
+    }
+}
+
+export async function deleteImg( url ){
+    const httpReference = await ref( storage, url );
+
+    try {
+        await deleteObject( httpReference );
+    } catch (error) {
+        console.log("ERROR delete img:", error);
+        return error;
+    }
 
 }
