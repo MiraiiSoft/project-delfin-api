d43a596b77345a59292d9982931b4efee185110e
ErickA2000 <erickwladimirarevalonavarro@gmail.com>
2023-06-22 09:55:00 -0600

    avances creacion servicio subida imagenes

--- src/services/firebase.js
+++ src/services/firebase.js
@@ -1,4 +1,10 @@
-import firebase from "firebase/compat/app"
-import "firebase/compat/storage"
-import { getStorage, ref, deleteObject } from "firebase/storage"
+import firebase from "firebase/compat/app";
+import "firebase/compat/storage";
+import { getStorage, ref, deleteObject } from "firebase/storage";
 
+const storageRef = firebase.app().storage().ref();
+const storage = getStorage();
+
+export async function uploadImg( nameFolde, nameImg, imgBase64 ){
+
+}
