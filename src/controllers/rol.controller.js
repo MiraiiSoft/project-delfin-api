67eb933cd7beb3664321c54d82cf420a4c3246e4
Javiss Negrete <negretbtino@hotmail.com>
2023-06-30 11:42:13 -0600

    Crud en roles

--- src/controllers/rol.controller.js
+++ src/controllers/rol.controller.js
@@ -1,26 +1,91 @@
 import { createRol, deleteRolById, getRolById, getRoles } from "../DAO/roll.dao.js";
 
 export const getAllRoles = async ( req, res ) => {
-    const roles = getRoles()
-    res.json(roles);
+    try {
+        const roles = await getRoles()
+        console.log("Peticion exitosa")
+        res.status(CODES_HTTP.OK).json({
+            success: true,
+            message: "Peticion exitosa:",
+            data: roles
+        });
+    } catch (error) {
+        console.log("Error al obtener los roles:", error)
+        return res.status(CODES_HTTP.INTERNAL_SERVER_ERROR).json({
+            success: false,
+            message: "Error al obtener los roles: " + error 
+        });
+    }
 }
 
 export const getOneRol = async ( req, res ) => {
-    const oneRol = getRolById(req.params)
-    res.json(oneRol);
+    try {
+        const oneRol = await getRolById(req.params.rolID)
+        console.log("Peticion exitosa")
+        res.status(CODES_HTTP.OK).json({
+            success: true,
+            message: "Peticion exitosa:",
+            data: oneRol
+        });
+    } catch (error) {
+        console.log("Error al obtener el rol:", error)
+        return res.status(CODES_HTTP.INTERNAL_SERVER_ERROR).json({
+            success: false,
+            message: "Error al obtener el rol:" + error 
+        });
+    }
 }
 
 export const addRol = async ( req, res ) => {
-    const newRol = createRol(req.body);
-    res.json(newRol);
+    try {
+        const newRol = await createRol(req.body)
+        console.log("Peticion exitosa")
+        res.status(CODES_HTTP.CREATED).json({
+            success: true,
+            message: "Peticion exitosa:",
+            data: newRol
+        });
+    } catch (error) {
+        console.log("Error al crear rol:", error)
+        return res.status(CODES_HTTP.INTERNAL_SERVER_ERROR).json({
+            success: false,
+            message: "Error al crear rol:" + error 
+        });
+    }
 }
 
 export const updateRol = async ( req, res ) => {
-    const updatedRol = updateRol(req.params, req.body)
-    res.json(updatedRol);
+    try {
+        const updatedRol = await updateRol(req.params.rolID, req.body)
+        console.log("Peticion exitosa")
+        res.status(CODES_HTTP.OK).json({
+            success: true,
+            message: "Peticion exitosa:",
+            data: updateRol
+        });
+    } catch (error) {
+        console.log("Error al actualizar rol:", error)
+        return res.status(CODES_HTTP.NO_FOUND).json({
+            success: false,
+            message: "Error al actualizar rol:" + error 
+        });
+    }
 }
 
 export const deleteRol = async ( req, res ) => {
-    const deletedRol = deleteRolById(req.params)
-    res.json(deletedRol);
+    try {
+        const deletedRol = await deleteRolById(req.params.rolID)
+        console.log("Peticion exitosa")
+        res.status(CODES_HTTP.OK).json({
+            success: true,
+            message: "Peticion exitosa:",
+            data: deleteRol
+        });
+    } catch (error) {
+        console.log("Error al eliminar rol:", error)
+        return res.status(CODES_HTTP.NO_FOUND).json({
+            success: false,
+            message: "Error al eliminar rol:" + error 
+        });
+    }
 }
