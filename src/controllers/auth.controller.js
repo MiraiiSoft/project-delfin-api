0cc68302939b9b3f90e559518d144aa4fb772bc0
ErickA2000 <erickwladimirarevalonavarro@gmail.com>
2023-06-30 11:39:39 -0600

    hecho helper para hashear contraseña

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -2,29 +2,29 @@ import { CODES_HTTP } from "../constants/global.js";
 import { createDirecciones } from "../DAO/direccion.dao.js";
 import { createPersona } from "../DAO/persona.dao.js";
 import { createLogin } from "../DAO/login.dao.js";
-
+import { hashPass,comparePass } from "../helpers/hashPass.js";
 
 export const register = async ( req, res ) => {
     try {
         
         const { nombre, apellido, telefono, correo, usuario, contraseña } = req.body;
+        
+        // const direccion = await createDirecciones({
+        //     codigo_postal: "0",
+        //     calle: "",
+        //     colonia: "",
+        //     num: "",
+        //     telefono,
+        //     referencia: "",
+        //     id_ciudad: 1
+        // });
 
-        const direccion = await createDirecciones({
-            codigo_postal: "0",
-            calle: "",
-            colonia: "",
-            num: "",
-            telefono,
-            referencia: "",
-            id_ciudad: 1
-        });
-
-        const persona = createPersona({
-            nombre,
-            apellido,
-            telefono,
-            id_direccion: direccion.id_direccion
-        });
+        // const persona = createPersona({
+        //     nombre,
+        //     apellido,
+        //     telefono,
+        //     id_direccion: direccion.id_direccion
+        // });
 
         res.status(CODES_HTTP.OK).json({
             success: true,
