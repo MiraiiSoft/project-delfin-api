5c79e9c09e88e24ad46c7027d422b441a82e13f3
ErickA2000 <erickwladimirarevalonavarro@gmail.com>
2023-06-30 11:25:52 -0600

    creando funcion de registro

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -1,5 +1,41 @@
-export const register = async ( req, res ) => {
+import { CODES_HTTP } from "../constants/global.js";
+import { createDirecciones } from "../DAO/direccion.dao.js";
+import { createPersona } from "../DAO/persona.dao.js";
+import { createLogin } from "../DAO/login.dao.js";
 
+
+export const register = async ( req, res ) => {
+    try {
+        
+        const { nombre, apellido, telefono, correo, usuario, contraseña } = req.body;
+
+        const direccion = await createDirecciones({
+            codigo_postal: "0",
+            calle: "",
+            colonia: "",
+            num: "",
+            telefono,
+            referencia: "",
+            id_ciudad: 1
+        });
+
+        const persona = createPersona({
+            nombre,
+            apellido,
+            telefono,
+            id_direccion: direccion.id_direccion
+        });
+
+        res.status(CODES_HTTP.OK).json({
+            success: true,
+            message: "Registro exictoso"
+        });
+    } catch (error) {
+        return res.status(CODES_HTTP.INTERNAL_SERVER_ERROR).json({
+            success: false,
+            message: "A ocurrido un error: " + error
+        })
+    }
 }
 
 export const login = async ( req, res ) => {
--- src/controllers/usuario.controller.js
+++ src/controllers/usuario.controller.js
@@ -1,5 +1,13 @@
+import { getPersonas } from "../DAO/persona.dao.js";
+import { CODES_HTTP } from "../constants/global.js";
+
 export const getAllUser = async ( req, res ) => {
-    
+    const personas = await getPersonas();
+
+    res.status(CODES_HTTP.OK).json({
+        success: true,
+        data: personas
+    })
 }
 
 export const getOneUser = async ( req, res ) => {
@@ -7,7 +15,7 @@ export const getOneUser = async ( req, res ) => {
 }
 
 export const addUser = async ( req, res ) => {
-
+    
 }
 
 export const updateUser = async ( req, res ) => {
