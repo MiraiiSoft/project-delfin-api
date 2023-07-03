0e3d485bf54198d1b4edbcebf0bc5378429de8b1
ErickA2000 <erickwladimirarevalonavarro@gmail.com>
2023-07-03 11:35:50 -0600

    realizando controldor de usuario

--- src/controllers/usuario.controller.js
+++ src/controllers/usuario.controller.js
@@ -1,17 +1,58 @@
-import { getPersonas } from "../DAO/persona.dao.js";
 import { CODES_HTTP } from "../constants/global.js";
+import { getPersonas, getPersonaById } from "../DAO/persona.dao.js";
+import { getLoginById, getLogins } from "../DAO/login.dao.js";
+import { getRolById } from "../DAO/roll.dao.js";
 
 export const getAllUser = async ( req, res ) => {
-    const personas = await getPersonas();
+    try {
+        let users = [];
 
-    res.status(CODES_HTTP.OK).json({
-        success: true,
-        data: personas
-    })
+        const userLogins = await getLogins();
+
+        for( let userLogin of userLogins ){
+            const user = await getPersonaById( parseInt(userLogin.id_persona) );
+            const roll = await getRolById( parseInt( userLogin.id_roll ) );
+
+            userLogin.id_persona = user;
+            userLogin.id_roll = roll;
+
+            users.push(userLogin);
+        }
+    
+        res.status(CODES_HTTP.OK).json({
+            success: true,
+            data: userLogins
+        });
+    } catch (error) {
+        return res.status(CODES_HTTP.INTERNAL_SERVER_ERROR).json({
+            success: false,
+            message: "A ocurrido un error:" + error
+        });
+    }
+
 }
 
 export const getOneUser = async ( req, res ) => {
+    const { userID } = req.params;
+    try {
+        const userLogin = await getLoginById( parseInt(userID) );
+        const user = await getPersonaById( parseInt(userLogin.id_persona) );
+        const roll = await getRolById( parseInt(userLogin.id_roll) );
 
+        userLogin.id_persona = user;
+        userLogin.id_roll = roll;
+
+        res.status(CODES_HTTP.OK).json({
+            success: true,
+            data: userLogin
+        })
+
+    } catch (error) {
+        return res.status(CODES_HTTP.INTERNAL_SERVER_ERROR).json({
+            success: false,
+            message: "A ocurrido un error: " + error
+        })
+    }
 }
 
 export const addUser = async ( req, res ) => {
