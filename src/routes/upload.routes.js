804c37ce62fbcb9204657435aaf7d67c374c89dd
ErickA2000 <erickwladimirarevalonavarro@gmail.com>
2023-07-05 11:50:00 -0600

    realizando pruebas documentacion

--- src/routes/upload.routes.js
+++ src/routes/upload.routes.js
@@ -4,7 +4,40 @@ import { readRequest, validation } from "../middlewares/index.js";
 
 const router = Router();
 
-
+/**
+* @openapi
+*tags:
+*   - name: Upload
+*  description: Endpoint para subir archivos
+* paths:
+*  /api/file/upload-img
+*      post:
+*         tags:
+*            - Upload
+*         summary: Subir imagenes
+*         requestBody:
+*            required: true
+*            content:
+*              application/json:
+*                 schema:
+*                     type: Object
+*                     properties:
+*                         nombreCarpeta:
+*                              type: string
+*                              example: Lapiz
+*                         imgs:
+*                              type: array
+*                              itmes:
+*                                  type: object
+*                                  properties:
+*                                      nombre:
+*                                          type: string
+*                                          example: Lapiz b1
+*                                      base64:    
+*                                          type: string
+*                                          example: adafadadad
+*
+*/
 
 
 
