a788fbc6642678ed68f286076c80c21004250ce4
ErickA2000 <erickwladimirarevalonavarro@gmail.com>
2023-07-06 10:12:33 -0600

    cambios en payment

--- src/helpers/getNextSequenceValue.js
+++ src/helpers/getNextSequenceValue.js
@@ -1,5 +1,15 @@
-import { updateCounter } from "../DAO/counter.dao.js";
+import { getCounterById,updateCounter } from "../DAO/counter.dao.js";
 
 const getNextSeqValue = async ( sequenceName ) => {
-    const sequence = await updateCounter(  )
+    try {
+        console.log("seq name:", sequenceName)
+        const valueSeq = await getCounterById(sequenceName);
+        // const sequence = await updateCounter(  )
+        return valueSeq;
+    } catch (error) {
+        console.log("Error seq value:", error)
+        return null;
+    }
 }
+
+export default getNextSeqValue;
