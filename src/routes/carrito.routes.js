8e65b57c2e188851365a1ce7707184e737610f62
ErickA2000 <erickwladimirarevalonavarro@gmail.com>
2023-07-31 12:56:38 -0600

    agregada nueva ruta de cart

--- src/routes/carrito.routes.js
+++ src/routes/carrito.routes.js
@@ -1,11 +1,12 @@
 import { Router } from "express";
-import { addCarrito, deleteCarrito, getAllCarritos, getOneCarrito, updateCarrito } from "../controllers/carrito.controller.js";
-import { cleanerRequest, validationCarrito, validationCarritoProducto } from "../middlewares/index.js";
+import { addCarrito, deleteCarrito, getAllCarritos, getOneCarrito, updateCarrito, getOneCarritoByUser } from "../controllers/carrito.controller.js";
+import { cleanerRequest, validationCarrito, validationCarritoProducto, authenticationJWT } from "../middlewares/index.js";
 
 const router = Router();
 
 router.get( '/', getAllCarritos );
 router.get('/:cartID', [ validationCarrito.noExistId ] , getOneCarrito);
+router.get( '/user/login', [ authenticationJWT.tokenValidation ], getOneCarritoByUser );
 router.post( '/add', [ validationCarrito.existName, cleanerRequest.carrito ] ,addCarrito );
 router.put( '/update/:cartID', [ validationCarritoProducto.noExistId ],updateCarrito );
 router.delete( '/delete/:cartID', [ validationCarrito.noExistId ] , deleteCarrito );
