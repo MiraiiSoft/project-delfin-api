2da7327f1086c76838d5f41ec299ac9678e0a23d
Javiss Negrete <negretbtino@hotmail.com>
2023-08-03 08:10:50 -0600

    Fixed problems

--- src/middlewares/Validations/carrito_producto.validation.js
+++ src/middlewares/Validations/carrito_producto.validation.js
@@ -16,7 +16,7 @@ export const noExistId = async ( req, res, next ) => {
 export const test = async ( req, res, next ) => {
     const { id_producto, id_carrito, cantidad_producto } = req.body
     const query = await getCarritoById(id_carrito)
-    var stop = false
+    var existProduct = false
 
     if ( query ) {
         query.carrito_producto.forEach(carrito_producto => {
@@ -26,12 +26,12 @@ export const test = async ( req, res, next ) => {
                 updateCarritoProductos( carrito_producto.id_carrito_producto, carrito_producto)
                     .catch( e => ( e.BAD_REQUEST ))
 
-                stop = true
+                existProduct = true
             }
         });
     }
 
-    if (stop)
+    if (existProduct)
         return res.status(CODES_HTTP.OK).json({
             success: true,
             message: "Se han agregado los productos a carrito-producto con id=" +id_carrito+ " correctamente",
